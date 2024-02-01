757c6925745d4a80d9d0f5921924223df79bd3c7
Ilyas Iusupov <iliaiusupov@gmail.com>
2024-02-01 13:41:04 -0600

    added Zero DownTime dependence

--- elb/main.tf
+++ elb/main.tf
@@ -2,18 +2,18 @@ resource "aws_lb" "app_alb" {
   name               = "app-alb-${var.environment}"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [module.security.alb_sg_id]
+  security_groups    = [aws_security_group.alb_sg.id]
   subnets            = module.vpc.public_subnets_ids
 
-  enable_deletion_protection = false
+  lifecycle {
+    create_before_destroy = true
+  }
 
-  tags = merge(
-    var.common_tags,
-    {
-      "Name" = "app-alb-${var.environment}"
-    }
-  )
+  tags = {
+    Name = "app-alb-${var.environment}"
+  }
 }
+
 resource "aws_lb_target_group" "app_tg" {
   name     = "app-tg-${var.environment}"
   port     = 80
@@ -45,5 +45,24 @@ resource "aws_lb_listener" "app_listener" {
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.app_tg.arn
+  }
+}
+resource "aws_autoscaling_group" "app" {
+  launch_template {
+    id      = aws_launch_template.app.id
+    version = "$Latest"
+  }
+
+  min_size         = 1
+  max_size         = 10
+  desired_capacity = 2
+  vpc_zone_identifier = ["subnet-12345678", "subnet-87654321"]
+
+  target_group_arns = [aws_lb_target_group.app.arn]
+
+  tag {
+    key                 = "Name"
+    value               = "app-instance"
+    propagate_at_launch = true
   }
 }
